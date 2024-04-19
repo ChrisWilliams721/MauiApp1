139a3905cafa500451a91343f4b2a8f0ffd6be08
Chris Williams <cchriswilliams721@gmail.com>
2024-04-19 12:53:19 -0600

    Add project files.

---
+++ MauiApp1/Components/Customer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Components
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int VAT_ID_number { get; set; }
+        public DateTime Current_Date { get; set; }
+        public string Email { get; set; }
+        public int Phone_Number { get; set; }
+        public string Phone_NumberToString
+        {
+            get => Phone_Number.ToString();
+            set
+            {
+
+                if (int.TryParse(value, out int result))
+                {
+                    Phone_Number = result;
+                }
+                else
+                {
+
+                }
+            }
+        }
+        public string VAT_ID_numberToString
+        {
+            get => VAT_ID_number.ToString();
+            set
+            {
+
+                if (int.TryParse(value, out int result))
+                {
+                    VAT_ID_number = result;
+                }
+                else
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ MauiApp1/Components/Equipment.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Components
+{
+    public class Equipment
+    {
+        public string Equipment_Name { get; set; }
+        public int Equipment_ID { get; set; }
+        public string Equipment_IDToString
+        {
+            get => Equipment_ID.ToString();
+            set
+            {
+
+                if (int.TryParse(value, out int result))
+                {
+                    Equipment_ID = result;
+                }
+                else
+                {
+
+                }
+            }
+        }
+        
+        public int Category_ID { get; set; }
+        public string Category_IDToString
+        {
+            get => Category_ID.ToString();
+            set
+            {
+
+                if (int.TryParse(value, out int result))
+                {
+                    Category_ID = result;
+                }
+                else
+                {
+
+                }
+            }
+        }
+        public int Daily_Rate { get; set; }
+        public string Daily_RateToString
+        {
+            get => Daily_Rate.ToString();
+            set
+            {
+
+                if (int.TryParse(value, out int result))
+                {
+                    Daily_Rate = result;
+                }
+                else
+                {
+
+                }
+            }
+        }
+        
+        public string Description { get; set; }
+       
+    }
+}
